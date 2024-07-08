406886cd528914937e5b6b0ffb5b3c8f96fc2a7c
Manolopeakucab <mapico.20@est.ucab.edu.ve>
2024-07-08 14:15:31 -0400

    docker added

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,12 @@ from flask_session import Session
 
 app = Flask(__name__, template_folder='../templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Maristas18@localhost/test5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:manuel@localhost/pptbd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 db = SQLAlchemy(app)
 Session(app)
@@ -16,5 +19,5 @@ socketio = SocketIO(app)  # Initialize SocketIO here
 
 from app import routes
 
-#with app.app_context():
-#    db.create_all()
+with app.app_context():
+   db.create_all()
