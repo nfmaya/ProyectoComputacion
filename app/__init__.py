17b4f09f5ce421ccaea256a53a1fdf618733106e
Samuel De Andrade <101124153+Butish@users.noreply.github.com>
2024-07-09 16:07:38 -0400

    Correcion de errores del front

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_session import Session
 
 app = Flask(__name__, template_folder='../templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Maristas18@localhost/test5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0904@localhost:5432/prueba'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 
